4c8715a9b9631043f9992df8f2ef08f18f0a72b4
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-28 00:20:10 +0200

    Implement getRoot() method

--- src/main/java/com/codecool/BinarySearchTree.java
+++ src/main/java/com/codecool/BinarySearchTree.java
@@ -8,14 +8,35 @@ public class BinarySearchTree {
     }
 
     public void initialize(Integer[] array) {
+        initializeRoot(array);
+    }
 
+    private void initializeRoot(Integer[] array) {
+        int rootValue = getMiddleValue(array);
+        root = createNewNode(rootValue);
+    }
+
+    private Node createNewNode(int rootValue) {
+        return new Node(rootValue);
+    }
+
+    private int getMiddleValue(Integer[] array) {
+        return array.length / 2;
     }
 
     public int getRoot() {
-        return 0;
+        return getNodeValue(root);
+    }
+
+    private int getNodeValue(Node node) {
+        return node.value;
     }
 
     private class Node {
+        private int value;
 
+        Node(int value) {
+            this.value = value;
+        }
     }
 }
