6b5a1ae6b35ace027afd348c304009732c0ab134
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-28 00:07:32 +0200

    Implement early version of class

--- src/main/java/com/codecool/BinarySearchTree.java
+++ src/main/java/com/codecool/BinarySearchTree.java
@@ -1,4 +1,21 @@
 package com.codecool;
 
 public class BinarySearchTree {
+    Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void initialize(Integer[] array) {
+
+    }
+
+    public int getRoot() {
+        return 0;
+    }
+
+    private class Node {
+
+    }
 }
