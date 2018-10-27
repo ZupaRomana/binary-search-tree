2bbbd4066e2af5b80b7a0038543eaad51822f966
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-28 00:03:05 +0200

    Add test checking getRoot() method

--- src/test/java/com/codecool/BinarySearchTreeTest.java
+++ src/test/java/com/codecool/BinarySearchTreeTest.java
@@ -1,7 +1,19 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTreeTest {
+    private Integer[] array = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    @Test
+    void returnsValidRoot() {
+        BinarySearchTree tree = new BinarySearchTree(array);
+
+        int expected = 5;
+        int actual = tree.getRoot();
+
+        assertEquals(expected, actual);
+    }
 }
